03cd2834c6e13339dfa5376f8a2075fa5c183402
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-10-16 10:37:19 +0800

    remove project

--- machine_learning/paddle_detection/main.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo11n-pose.pt")  # load a pretrained model (recommended for training)
-
-results = model.predict(source="source.mp4", show=True, save=True)
